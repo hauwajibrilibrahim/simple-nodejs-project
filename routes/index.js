273eca7df0d4d9169ff29f24fd56a892f3d64ec9
Hauwa Jibril Ibrahim <hauwajibrilibrahim@gmail.com>
2021-01-07 13:10:05 +0000

    Created GET and POST API's

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
 var express = require("express");
+const { render } = require("pug");
 var router = express.Router();
-let index = require("../controllers/index");
-/* GET home page. */
-router.get("/", index.index);
 
+var data = "Any string";
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+router.get("//", (req, res) => {
+  console.log("// endpoint called");
+  res.json({
+    status: "success",
+  });
+});
+router.post("/data", (req, res) => {
+  req.body.data;
+  res.json({ data });
+});
+router.get("/data", (req, res) => {
+  console.log("/data endpoint called");
+  res.json({ data });
+});
 module.exports = router;
